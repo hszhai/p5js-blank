9c7219a09cc737455bfefee915907842e1b4c344
Sean Zhai <hszhai@syr.edu>
2024-03-10 10:25:47 -0400

    Update sketch.js (eslint highlighting)

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+/*global THRESHOLD,INVERT,BLUR,POSTERIZE,square,circle,quad,HALF_PI,arc,pmouseX,pmouseY,map,saveSound,p5,noTint,tint,GRAY,filter,lerpColor,resizeCanvas,CLOSE,vertex,rectMode,CORNER,RGB,ARROW,HAND,cursor,createSlider,createButton,rect,HSB,colorMode,ambientMaterial,pop,cylinder,specularMaterial,radians,rotateX,rotateY,rotateZ,push,sphere,directionalLight,texture,orbitControl,createGraphics,WEBGL,createCanvas,color,translate,triangle,frameRate,beginShape,endShape,curveVertex,shuffle,sin,cos,floor,rotate,textAlign,LEFT,RIGHT,CENTER,text,textSize,stroke,noStroke,strokeWeight,keyCode,keyIsDown,LEFT_ARROW,RIGHT_ARROW,UP_ARROW,DOWN_ARROW,mouseIsPressed,fill,noFill,mouseX,mouseY,line,ellipse,background,displayWidth,displayHeight,windowWidth,windowHeight,height,width,dist,loadSound,loadImage,image,random,angleMode,RADIANS,DEGREES,frameCount,millis,round*/
+
 function setup() {
   createCanvas(400, 400);
 }
